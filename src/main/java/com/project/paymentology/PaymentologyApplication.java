bfd850d34b70e2116763386f5076a792b4bce5cf
Ming <lxm2133@yahoo.com>
2022-05-30 01:09:39 +0800

    project initialization

---
+++ src/main/java/com/project/paymentology/PaymentologyApplication.java
@@ -0,0 +1,13 @@
+package com.project.paymentology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentologyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/paymentology/PaymentologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.paymentology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentologyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
